da77b651d62a1351e11af7df050986caf625ab5a
Rafael Rocha <rafael.rocha.mg@gmail.com>
2021-11-17 08:34:33 -0300

    test: customer feature test

---
+++ tests/Feature/CustomersApiTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\City;
+use App\Models\Company;
+use App\Models\Customer;
+use App\Models\Gender;
+use Tests\TestCase;
+
+class CustomersApiTest extends TestCase
+{
+    public function test_unauthorized_customers_request()
+    {
+        $response = $this->getJson('/api/customers');
+        $response->assertStatus(401);
+    }
+
+    public function test_unauthorized_customer_request()
+    {
+        $response = $this->getJson('/api/customers/1');
+        $response->assertStatus(401);
+    }
+
+    public function test_authorized_customers_request()
+    {
+        $accessToken = $this->postJson(
+            '/api/register',
+            [
+                "name"                  => "user",
+                "email"                 => "user@mail.com",
+                "password"              => "12345678",
+                "password_confirmation" => "12345678"
+            ]
+        )->json();
+        $gender = Gender::create(['description' => 'Female']);
+        $company = Company::create(['name' => 'My company']);
+        $city = City::create(['name' => 'My city', 'state' => 'AZ']);
+        $customer = Customer::create(
+            [
+                'first_name' => 'user',
+                'last_name'  => 'test',
+                'email'      => 'test@email.com',
+                'gender_id'  => $gender->id,
+                'company_id' => $company->id,
+                'city_id'    => $city->id
+            ]
+        );
+        $response = $this->withHeader('Authorization', 'Bearer ' . $accessToken)
+            ->getJson('/api/customers/' . $customer->id);
+        $response->assertStatus(200);
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
+    use CreatesApplication, DatabaseMigrations;
+
+    protected static $initialized = false;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        if (!self::$initialized) {
+            Artisan::call('migrate',['-vvv' => true]);
+            Artisan::call('passport:install',['-vvv' => true]);
+            self::$initialized = true;
+        }
+    }
 }
